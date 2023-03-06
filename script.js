af434ee8229ae4d4f28aacd50a8dc8243b3243af
sayeedhj <sayeedhasanjisad>
2023-03-06 17:34:23 +0600

    another-commit

--- script.js
+++ script.js
@@ -30,3 +30,23 @@ var tablinks = document.getElementsByClassName("tab-links");
     backSpeed: 70,
     loop: true
    })
+
+
+
+   const scriptURL = 'https://script.google.com/macros/s/AKfycbyA7SjKAom-XkynnRKLTEvBOr-OdGnEFhiRUKrHINLThO9TPdRY1rEVUmsCaZPPT_Y1WQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent Successfully"
+        setTimeout(function(){
+            msg.innerHTML = "";
+        }, 5000)
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  
+  })
